c3d731f0e6c54e7d1a78d97b32fc2e36f2a9d6e3
Dom Sec <54043553+dom-sec@users.noreply.github.com>
2023-03-20 21:12:03 -0400

    Added password_generator files

---
+++ password_generator/src/main.rs
@@ -0,0 +1,19 @@
+use rand::Rng;
+
+fn main() {
+    
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789)(*&^%$#@!~";
+    const PASSWORD_LEN: usize = 30;
+    let mut rng = rand::thread_rng();
+
+    let password: String = (0..PASSWORD_LEN)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+    println!("The following password was generated:");
+    println!("{:?}", password);
+}
