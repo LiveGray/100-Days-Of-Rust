f1726231bc2f121ab87be85cac24343b66aaa656
Smorci <szekelyschmarton@gmail.com>
2024-02-14 20:22:10 +0000

    Initial commit

---
+++ Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/target/debug/deps/convert_ages_to_days-50ff0c90effb7e23.d
@@ -0,0 +1,5 @@
+/home/smorci/Documents/personal_code/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/target/debug/deps/libconvert_ages_to_days-50ff0c90effb7e23.rmeta: src/main.rs
+
+/home/smorci/Documents/personal_code/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/target/debug/deps/convert_ages_to_days-50ff0c90effb7e23.d: src/main.rs
+
+src/main.rs:
---
+++ Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/target/debug/deps/convert_ages_to_days-6b5789ab59e844ba.d
@@ -0,0 +1,5 @@
+/home/smorci/Documents/personal_code/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/target/debug/deps/libconvert_ages_to_days-6b5789ab59e844ba.rmeta: src/main.rs
+
+/home/smorci/Documents/personal_code/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days/target/debug/deps/convert_ages_to_days-6b5789ab59e844ba.d: src/main.rs
+
+src/main.rs:
