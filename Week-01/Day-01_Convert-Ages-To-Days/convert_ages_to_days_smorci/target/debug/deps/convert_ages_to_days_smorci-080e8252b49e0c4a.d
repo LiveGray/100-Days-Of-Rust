ef2d73efe5b0ee0c8f4be2b6aab532057dccab72
Smorci <szekelyschmarton@gmail.com>
2024-02-14 20:58:32 +0000

    Delete smorci directory, disable on push nix build, direnv

--- Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/target/debug/deps/convert_ages_to_days_smorci-080e8252b49e0c4a.d
+++
@@ -1,5 +0,0 @@
-/home/smorci/Documents/personal_code/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/target/debug/deps/libconvert_ages_to_days_smorci-080e8252b49e0c4a.rmeta: src/main.rs
-
-/home/smorci/Documents/personal_code/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/target/debug/deps/convert_ages_to_days_smorci-080e8252b49e0c4a.d: src/main.rs
-
-src/main.rs:
--- Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/target/debug/deps/convert_ages_to_days_smorci-6f79d982c26249a1.d
+++
@@ -1,5 +0,0 @@
-/home/smorci/Documents/personal_code/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/target/debug/deps/libconvert_ages_to_days_smorci-6f79d982c26249a1.rmeta: src/main.rs
-
-/home/smorci/Documents/personal_code/100-Days-Of-Rust/Week-01/Day-01_Convert-Ages-To-Days/convert_ages_to_days_smorci/target/debug/deps/convert_ages_to_days_smorci-6f79d982c26249a1.d: src/main.rs
-
-src/main.rs:
